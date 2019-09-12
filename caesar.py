6c3f1b994cb4f8896b0924b109eac3aa1a0925e2
Cooper deNicola <cdenicol@stanford.edu>
2019-09-12 12:23:59 -0700

    Update caesar.py output formatting

--- caesar.py
+++ caesar.py
@@ -1,6 +1,5 @@
 #Preforms a basic caesar shift, then prints output
 def caesar(instr, shift):
-    #print("Input String:", instr)
     instr = instr.upper()
     newstr = ""
     for char in instr:
@@ -9,10 +8,11 @@ def caesar(instr, shift):
             continue
         ltr = ord(char) - 65
         newstr += chr(((ltr + shift) % 26) + 65)
-    print("Ciphered String:", newstr)
+    print("Shifted by ", shift, ": ", newstr, sep="")
 
 
-# caesar("efgfoe uif fbtu xbmm pg uif dbtumf", -1)
+
+#caesar("efgfoe uif fbtu xbmm pg uif dbtumf", -1)
 # caesar(numToStr("""4	18	15	 	13	19	11	 	19	3
 # 22	25	13	11	4 15 14	 	19	24	 	22	11	24	17	22	15	9
 # 6	11.	""", "	 	"), 15)
